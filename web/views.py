07e2ee114b5adb0ff90af15b931ae3ab5651db45
kum83 <momo821123@outlook.com>
2021-08-23 07:18:33 +0300

    f

--- web/views.py
+++ web/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import whois
-import os
+# import whois
 import subprocess
 
 
@@ -16,9 +15,7 @@ def whois_ip(request):
             result = subprocess.check_output(batcmd, shell=True).decode("utf-8") 
             resp_json = {}
             last_key = ""
-            print("========== result ")
-            print(result)
-            # result = os.system("whois {}".format(ip_addr))
+
             for line in result.splitlines():
                 if line.find(">>>") == 0: break
                 sublines = line.split(": ", 1)
@@ -31,17 +28,4 @@ def whois_ip(request):
                 else:
                     if last_key != "" :
                         resp_json[last_key] += "\n" + line
-
-            # ip_result = whois.whois(ip_addr)
-            # response = {
-            #     'domain_name':ip_result.domain_name,
-            #     'org':ip_result.org,
-            #     'emails':ip_result.emails,
-            #     'state':ip_result.state,
-            #     'country':ip_result.country,
-            #     'domain_name':ip_result.domain_name,
-            #     'registrar':ip_result.registrar,
-            #     'expiration_date':ip_result.expiration_date,
-            #     'name_servers':ip_result.name_servers
-            #     }
     return JsonResponse(resp_json)
